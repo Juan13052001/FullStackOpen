068b3cd06aafc705379566b771c3881d8c5eda57
Juan <jherrerav1901@alumno.ipn.mx>
2022-10-01 23:08:47 -0500

    Part1 iniciando seccion d

---
+++ part1/Unicafe/src/App.jsx
@@ -0,0 +1,9 @@
+import { useState } from "react";
+export const App = () => {
+    // save clicks of each button to its own state
+    const [good, setGood] = useState(0);
+    const [neutral, setNeutral] = useState(0);
+    const [bad, setBad] = useState(0);
+
+    return <div>code here</div>;
+};
