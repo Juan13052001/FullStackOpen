fc0b8804c56096040e43d2ad0671e5676a30e898
Juan <jherrerav1901@alumno.ipn.mx>
2022-10-01 22:45:24 -0500

    Part1 B Completado

--- part1/src/components/Content.jsx
+++ part1/src/components/Content.jsx
@@ -1,22 +1,15 @@
-export const Content = ({
-    part1,
-    part2,
-    part3,
-    exercises1,
-    exercises2,
-    exercises3,
-}) => {
+import { Part } from "./Part";
+
+export const Content = ({ parts }) => {
     return (
         <>
-            <p>
-                {part1} {exercises1}
-            </p>
-            <p>
-                {part2} {exercises2}
-            </p>
-            <p>
-                {part3} {exercises3}
-            </p>
+            {parts.map((part) => (
+                <Part
+                    part={part.name}
+                    exercise={part.exercises}
+                    key={part.name}
+                />
+            ))}
         </>
     );
 };
---
+++ part1/src/components/Part.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Part = ({ part, exercise }) => {
+    return (
+        <p>
+            {part} {exercise}
+        </p>
+    );
+};
